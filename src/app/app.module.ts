c3876f4348e10404b84e4073f83a6fa8fd93ac80
queren sales <querenhsales@gmail.com>
2022-01-29 00:53:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { PokemonItemComponent } from './pokemon-item/pokemon-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CabecalhoComponent,
+    PokemonItemComponent
   ],
   imports: [
     BrowserModule,
