b8959145329e2fd36bbca67e32e8fbe0309f75f7
Querensales <querenhsales@gmail.com>
2022-02-01 19:09:13 -0300

    Buscando pokemon

--- src/app/pokemon-item/pokemon-item.component.ts
+++ src/app/pokemon-item/pokemon-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { pokemonModel } from '../shared/pokemon.model';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-item',
@@ -11,23 +12,62 @@ export class PokemonItemComponent implements OnInit {
 
   pokemonList: pokemonModel[] = [];
 
+  nextUrl!: string;
+
+  previousUrl!: string;
+
+  quantidadePorPagina: number = 5;
+
+  quantidadeTotal: number = 0;
+
   constructor(private requisicao: HttpClient) { }
 
   ngOnInit(): void {
-    this.buscarPokemon();
+    let url: string = (`https://pokeapi.co/api/v2/pokemon?limit=${this.quantidadePorPagina}`);
+    this.buscarPokemon(url);
+
   }
 
-  buscarPokemon(): void {
-    let promessaPokemon = this.requisicao.get('https://pokeapi.co/api/v2/pokemon?limit=20');
+  buscarPokemon(urlApi: string): void {
+    console.log(this.previousUrl)
+    this.pokemonList = [];
+    this.previousUrl = '';
+    let promessaPokemon = this.requisicao.get(urlApi);
     promessaPokemon.subscribe((result: any) => {
+      this.quantidadeTotal = result.count;
+      this.nextUrl = result.next;
+      this.previousUrl = result.previous;
+
+
+
+
       let listaPokemonApi: any[] = result.results;
       listaPokemonApi.forEach(element => {
-        let pokemon = new pokemonModel();
-        pokemon.nome = element.name;
-
+        let pokemon = this.createPokemonModel(element);
         this.pokemonList.push(pokemon);
       });
     });
+  }
+
+  createPokemonModel(pokemonApi: any): pokemonModel {
+    let pokemon = new pokemonModel();
+    pokemon.nome = pokemonApi.name;
+
+
+    this.requisicao.get(pokemonApi.url)
+      .subscribe((detalhePokemonApi: any) => {
+        pokemon.caminhoImagem = detalhePokemonApi.sprites.other.dream_world.front_default;
+
+
+        let urlcaracteristica = detalhePokemonApi.moves[0].version_group_details[0].move_learn_method.url;
+
+        this.requisicao.get(urlcaracteristica)
+          .subscribe((caracteristicaApi: any) => {
+            pokemon.caracteristicas = caracteristicaApi.descriptions[0].description;
+          })
+      })
+
+    return pokemon;
   }
 
 
